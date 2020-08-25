d5570f6da6c7679d4304b588d63c6a8bfe283c2e
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2020-08-26 01:34:05 +0200

    Update stats-card.js
    Change line 170 from "title: `${encodeHTML(name)}'${apostrophe} GitHub Stats`," to "title: `Overview`,

--- src/cards/stats-card.js
+++ src/cards/stats-card.js
@@ -167,7 +167,7 @@ const renderStatsCard = (stats = {}, options = { hide: [] }) => {
 
   const apostrophe = ["x", "s"].includes(name.slice(-1)) ? "" : "s";
   const card = new Card({
-    title: `${encodeHTML(name)}'${apostrophe} GitHub Stats`,
+    title: `Overview`,
     width: 495,
     height,
     colors: {
